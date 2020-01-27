69c42aedca73cc028c259f1b6e7dc74a22d1510f
vibraniumforge <gregm9999@gmail.com>
2020-01-26 17:50:09 -0800

    Done.

--- solution.rb
+++ solution.rb
@@ -1,47 +1,19 @@
-def move_zero(arr)
-    zeros = 0
-    arr.reject!{ |num| zeros+= 1 if num.zero? }
-    zeros.times{ arr.push(0)}
-
-    puts arr
+def move_zeroes(nums)
+	nums.each do |num|
+		zeros = 0
+		nums.reject!{|x| zeros +=1 if x.zero?}
+		zeros.times{nums.push(0)}
+	end
 end
 
 
-# arr = [1,4,0,0,8]
-
-# move_zero(arr)
-
-# def sum_two(num, target)
-#     num_at = 0
-#     num.each do |first_number|
-#         num.each do |second_number|
-            
-#             if first_number < second_number
-#                 # puts "#{n} - #{m}"
-#                 num_at = first_number + second_number
-#                 if num_at == target
-#                     puts "index-1: #{num.index(first_number)} index-2: #{num.index(second_number)}"
-#                 end
-#             end
-#         end
-#     end 
-# end
-
-nums = [1,2,6,9,10]
-target = 12
-
-def two_sum(nums, target)
-    h = {}
-    nums.each_with_index do |value, index|
-        diff = target - value
-         if h[diff]
-            puts [h[diff], index]
-         end
-        h[value] = index
-    end
-    puts h
+def two_sum(numbers, target)
+    numbers.each do |first_num|
+			numbers.each do |second_num|
+				if first_num < second_num
+					if first_num + second_num == target
+						puts "#{first_num}, #{second_num}"
+				end
+			end
+		end
 end
-
-two_sum(nums,target)
-
-
